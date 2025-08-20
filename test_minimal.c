b5ff408dcaba3a2dc70a9a44521b9b48dce81bf5
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2025-08-20 16:34:19 -0400

    Simplify test_minimal.c to remove complex.h dependency
    - Remove complex.h include that may cause Windows MinGW issues
    - Keep C99 Variable Length Array test as primary feature test
    - Add compiler detection to verify MinGW is being used
    - Complete minimal Python extension module structure
    - This should resolve Windows compilation issues with complex numbers

--- test_minimal.c
+++ test_minimal.c
@@ -1,33 +1,57 @@
 /* test_minimal.c - Ultra-minimal C extension for Phase 1A testing
- * Goal: Test if C99 compilation works on all platforms
+ * Goal: Test if basic C compilation works on all platforms
+ * Note: Removed complex.h to avoid Windows MinGW issues
  */
 
 #include <Python.h>
-#include <complex.h>  // Test C99 complex.h support
-#include <stddef.h>
 
-/* Test function that uses C99 features */
+/* Test function that uses C99 Variable Length Arrays but not complex.h */
 static PyObject *test_c99_features(PyObject *self, PyObject *args) {
     int n = 10;
 
     /* Test C99 Variable Length Array (VLA) - this will fail on MSVC */
     double test_array[n];
 
-    /* Test C99 complex numbers */
-    double complex z = 1.0 + 2.0*I;
-    double real_part = creal(z);
-    double imag_part = cimag(z);
-
-    /* Initialize the VLA */
+    /* Initialize the VLA with simple arithmetic */
     for (int i = 0; i < n; i++) {
-        test_array[i] = (double)i + real_part;
+        test_array[i] = (double)i * 2.0;
     }
 
     /* Return a simple result */
-    return PyFloat_FromDouble(test_array[5] + imag_part);
+    return PyFloat_FromDouble(test_array[5]);
 }
 
 /* Test function to verify MinGW vs MSVC */
+static PyObject *test_compiler_check(PyObject *self, PyObject *args) {
+#ifdef __MINGW32__
+    return PyUnicode_FromString("MinGW");
+#elif defined(_MSC_VER)
+    return PyUnicode_FromString("MSVC");
+#else
+    return PyUnicode_FromString("Other");
+#endif
+}
+
+/* Module method definitions */
+static PyMethodDef test_methods[] = {
+    {"test_c99_features", test_c99_features, METH_NOARGS, "Test C99 VLA support"},
+    {"test_compiler_check", test_compiler_check, METH_NOARGS, "Check which compiler is being used"},
+    {NULL, NULL, 0, NULL}  /* Sentinel */
+};
+
+/* Module definition */
+static struct PyModuleDef test_module = {
+    PyModuleDef_HEAD_INIT,
+    "_test_minimal",        /* name of module */
+    "Ultra-minimal test module for Phase 1A",  /* module documentation */
+    -1,                     /* size of per-interpreter state of the module */
+    test_methods
+};
+
+/* Module initialization function */
+PyMODINIT_FUNC PyInit__test_minimal(void) {
+    return PyModule_Create(&test_module);
+}
 static PyObject *test_compiler_info(PyObject *self, PyObject *args) {
     const char *compiler_info;
 
